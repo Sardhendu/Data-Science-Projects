b46555492c7bae02014a4ec4ce9544d8d3168450
Sardhendu Mishra <sardhendumishra@gmail.com>
2017-03-31 18:29:17 -0500

    Adding R code changes

--- DiabeticReadmission-Rough.R
+++ DiabeticReadmission-Rough.R
@@ -1,4 +1,6 @@
 # Import libraries here
+rm(list = ls())
+
 library(dplyr)
 library(class)
 library(MASS)
@@ -10,7 +12,7 @@ library(rpart)
 library(lars)
 library(stats)
 library(leaps)
-# library(repr)
+library(repr)
 library(glmnet)
 library('caret')
 
@@ -20,60 +22,80 @@ df_Base = '/Users/sam/All-Program/App-DataSet/Spark-Operations/diabetes_dataset/
 df_F1 = '/Users/sam/All-Program/App-DataSet/Spark-Operations/diabetes_dataset/df_F1.csv/'
 df_F2 = '/Users/sam/All-Program/App-DataSet/Spark-Operations/diabetes_dataset/df_F2.csv/'
 
-df_Diag1Diag2NaN_rmv_F1 = '/Users/sam/All-Program/App-DataSet/Spark-Operations/diabetes_dataset/df_Diag1Diag2NaN_rmv_F1.csv/'
-df_Diag1Diag2NaN_rmv_F2 = '/Users/sam/All-Program/App-DataSet/Spark-Operations/diabetes_dataset/df_Diag1Diag2NaN_rmv_F2.csv/'
+df_Diag2Diag2NaN_rmv_F1 = '/Users/sam/All-Program/App-DataSet/Spark-Operations/diabetes_dataset/df_Diag1Diag2NaN_rmv_F1.csv/'
+df_Diag2Diag2NaN_rmv_F2 = '/Users/sam/All-Program/App-DataSet/Spark-Operations/diabetes_dataset/df_Diag1Diag2NaN_rmv_F2.csv/'
 
 
-file_names <- list.files(path = df_Diag1Diag2NaN_rmv_F1, pattern = ".csv", full.names = TRUE)
-dataF1_DF <- do.call(rbind,lapply(file_names, read.csv, header=TRUE))
-
-
-dataF1_DF = data.frame(dataF1_DF)
-head(dataF1_DF)
-
+runningDataSet <- df_F2
 
 ##################################################################################################
 
-# Do some small sanity check to be sure your data looks the same way as dumped before.
+# Check the data if its looks alright
+file_names <- list.files(path = runningDataSet, pattern = ".csv", full.names = TRUE)
+data_DF <- do.call(rbind,lapply(file_names, read.csv, header=TRUE))
+data_DF = data.frame(data_DF)
+# Check how many unique output labels we have, should be binomial (no, and yes)
+unique(data_DF$readmitted)
+head(data_DF)
+
+##################################################################################################
+
+getData <- function(dataIN, featureType){
+  # Do some small sanity check to be sure your data looks the same way as dumped before.
+  # Check for numeric and factor columns and maatch them, if the are proper
+  numCol <- length(names(dataIN))
+  print ('The Total number of featurer incluing the Label and ID columns are: ') 
+  numCol
+  
+  
+  # Check if the correct features are numeric, if not then convert them to numeric and vice-a-versa 
+  # c('time_in_hospital', 'num_lab_procedures', 'num_procedures', 'num_medications', 'number_outpatient', 'number_emergency', 'number_inpatient', 'number_diagnoses')
+  print ('All Nueric features interpreted by R are: ') 
+  names(which(sapply(dataIN, is.numeric)))
+  
+  # Check if the correct features are nominal, if not then convert them to factors and vice-a-versa
+  print ('All Factor features interpreted by R are: ') 
+  names(which(sapply(dataIN, is.factor)))
+  
+  # Converting the non-numeric columns into factors
+  dataIN$admission_type_id <- as.factor(as.character(dataIN$admission_type_id))
+  dataIN$discharge_disposition_id <- as.factor(as.character(dataIN$discharge_disposition_id))
+  
+  if (featureType == 'F1'){
+    dataIN$admission_source_id <- as.factor(as.character(dataIN$admission_source_id))
+  }
+  
+  # Converting readmitted into 1 and 0 where 1=yes and 0=no
+  dataIN$readmitted<- as.factor(ifelse(dataIN$readmitted=='no', 0, 1))
+  
+  # Get the Numeric anf Factor features 
+  numericCols <- names(which(sapply(dataIN, is.numeric)))
+  # numericCols <- numericCols[numericCols != 'id']              # Removing the ID column
+  factorCols <- names(which(sapply(dataIN, is.factor)))
+  
+  return (list(numericCols, factorCols, dataIN)) 
+}
 
 
-# Check for numeric and factor columns and maatch them, if the are proper
-numCol <- length(names(dataF1_DF))
-print ('The Total number of featurer incluing the Label and ID columns are: ') 
-numCol
+data_OUT <- getData(data_DF, 'F2')
 
-
-# Check if the correct features are numeric, if not then convert them to numeric and vice-a-versa 
-# c('time_in_hospital', 'num_lab_procedures', 'num_procedures', 'num_medications', 'number_outpatient', 'number_emergency', 'number_inpatient', 'number_diagnoses')
-print ('All Nueric features interpreted by R are: ') 
-names(which(sapply(dataF1_DF, is.numeric)))
-
-# Check if the correct features are nominal, if not then convert them to factors and vice-a-versa
-print ('All Factor features interpreted by R are: ') 
-names(which(sapply(dataF1_DF, is.factor)))
-
-# Converting the non-numeric columns into factors
-dataF1_DF$admission_type_id <- as.factor(as.character(dataF1_DF$admission_type_id))
-dataF1_DF$discharge_disposition_id <- as.factor(as.character(dataF1_DF$discharge_disposition_id))
-dataF1_DF$admission_source_id <- as.factor(as.character(dataF1_DF$admission_source_id))
-
-# Converting readmitted into 1 and 0 where 1=yes and 0=no
-dataF1_DF$readmitted<- as.factor(ifelse(dataF1_DF$readmitted=="yes", 1, 0))
-
-# Get the Numeric anf Factor features 
-numericCols <- names(which(sapply(dataF1_DF, is.numeric)))
-# numericCols <- numericCols[numericCols != 'id']              # Removind the ID column
-factorCols <- names(which(sapply(dataF1_DF, is.factor)))
-
-# Samity check
 print ('All numeric columns after DataType Conversion are: ')
+numericCols <- data_OUT[[1]]
 numericCols
 print ('All factor columns after DataType Conversion are: ')
+factorCols <- data_OUT[[2]]
 factorCols
+data_DFclean <- data_OUT[[3]]
+
+# data_DF$readmitted<- as.factor(ifelse(data_DF$readmitted=="yes", 1, 0))
+# Check if the 'yes' and 'no' of the readmitted columns are converted properly, should produce 0's and 1's
+unique(data_DFclean$readmitted)
+head(data_DFclean)
 
 
 ##################################################################################################
 
+########## Common Functions  ############
 
 bindModel <- function(yLabel, xFeatures=NULL, featureSet=NULL){
   # Automates the creation of feature model to be passed into an Classifier or Predictive Model
@@ -83,28 +105,23 @@ bindModel <- function(yLabel, xFeatures=NULL, featureSet=NULL){
   return (as.formula(paste(yLabel, "~", paste(xFeatures, collapse = '+ '))))
 }
 
-factorMatrixBuilder <- function (dataFrameIN, numericCols, factorCols, yLabel){
+factorToDummy_DF_Builder <- function (dataFrameIN, numericCols, factorCols, yLabel){
   # Creates a design matrix by expanding the factors to a set of dummy variables and interaction etc.
   xNumeric <- dataFrameIN[, numericCols]
   xFactor <- dataFrameIN[, factorCols]
   
   factorModel <- bindModel(yLabel=yLabel, featureSet=factorCols)
   xFactor <- model.matrix(factorModel, data=xFactor)[, -1]        # -1 is provided to exclude the intercept term from the matrix
-  
-  return (as.matrix(data.frame(xNumeric, xFactor)))
+  readmitted <- dataFrameIN[[yLabel]]
+  return (data.frame(xNumeric, xFactor, readmitted))
 }
 
-dim(dataF1_DF)
 
 stratifiedSampling <- function(dataIN, sample_on_col, trainPrcnt, seed){
   set.seed(seed)
-  print (dim(dataIN[sample_on_col]))
-  print (unique(dataIN[sample_on_col]))
   trainIndices <- createDataPartition(y=dataIN[[sample_on_col]], p=trainPrcnt, list=FALSE)
   trainData <- dataIN[trainIndices,]
   testData <- dataIN[-trainIndices,]
-  print (dim(trainData))
-  print (dim(testData))
   
   stopifnot(nrow(trainData) + nrow(testData) == nrow(dataIN))
   return (list(trainData, testData))
@@ -113,34 +130,57 @@ stratifiedSampling <- function(dataIN, sample_on_col, trainPrcnt, seed){
 
 ##################################################################################################
 
-dataOut <- stratifiedSampling(dataIN=dataF1_DF, sample_on_col='readmitted', trainPrcnt=0.8, seed=45223)
+# Build the design matrix by expanding the factor variables into dummy variables
+# We remove the columns 'diag_1', 'diag_2', 'diag_3' becasue they have any different categories and take much time to operate
+excludVariables <- c('diag_1', 'diag_2', 'diag_3')
+numericFeatures <- numericCols
+nominalFeatures <- factorCols
+nominalFeatures <- setdiff(nominalFeatures, excludVariables)
+nominalFeatures
 
-trainData <- dataOut[[1]]
-testData <- dataOut[[2]]
-
-dim(trainData)
-dim(testData)
+dataDF_dummy <- factorToDummy_DF_Builder(dataFrameIN=data_DFclean, numericCols=numericFeatures, factorCols=nominalFeatures, yLabel='readmitted')
+head(dataDF_dummy)
+print ('The number of instances are: ')
+nrow(dataDF_dummy)
+print ('The number of columns are; ')
+ncol(dataDF_dummy)
+# label <- dataMatrix$readmitted
+# as.matrix
 
 
 ##################################################################################################
+###################### Stratified Sampling #######################
+
+# Stratified Sampling
+dataOut <- stratifiedSampling(dataIN=dataDF_dummy, sample_on_col='readmitted', trainPrcnt=0.8, seed=45223)
+trainData <- dataOut[[1]]
+testData <- dataOut[[2]]
+print ('Total number of training points are: ')
+print (nrow(trainData))
+print ('Total number of test points are: ')
+print (nrow(testData))
+# Convert teh dataframes into matrix of dummyDF to feed into the algorithm
+# trainData <- as.matrix(trainData)
+# testData <- as.matrix(testData)
+# dataMatrix$readmitted
+head(trainData)
+head(testData)
 
 
 
-for (i in colnames(dataDF)){
-  print (i)
-  print (unique(dataDF[[i]]))
-}
-
+##################################################################################################
+###################### Common functions 2 #######################
 
 getSummaryElements_glm <- function(model, alpha){
   inputSummary <- summary(model)
   inputSummary_DF <- as.data.frame(inputSummary$coefficients)  # z is for glm
   inputSummary_DF = inputSummary_DF[-1,]   # Remove the summary for intercept
+  print (inputSummary_DF['Pr(>|z|)'])
   signFeatureIndice <- which(inputSummary_DF['Pr(>|z|)'] <= alpha)
   
-  numFeatures <- nrow(inputSummary_DF) -1 # -1 for the intercept
+  numFeatures <- nrow(inputSummary_DF) # -1 for the intercept
   numSignFeatures <- length(signFeatureIndice)
-
+  
   # Get the top i significant features
   i <- 1
   topI_SignFeatures <- 0
@@ -162,9 +202,7 @@ getSummaryElements_glm <- function(model, alpha){
     
     i <- i+1
   }
-
   
-  # print ('uoiweuifouweiofiowfoiof')
   # signFeatures <- row.names(inputSummary_DF[signFeatureIndice,])
   nullDeviance <- inputSummary$null
   residualDeviance <- inputSummary$deviance
@@ -194,13 +232,11 @@ getSummaryElements_glm <- function(model, alpha){
 }
 
 
-# inputSummary_DF <- as.data.frame(summary(modelF1.glm)$coefficients)['Pr(>|z|)']
-
-
-
 glmStats <- function(dataIN, max_p_val=0.20){
   cols <- colnames(dataIN)
   cols <- cols[1:length(cols)-1]   # removing readitted (the label from the list)
+  print ('The list of columns are: ')
+  print (cols)
   
   # stats <- c('numCategories', 'numSignCategories', 'signF1','signF2', 'signF3', 'nullDeviance', 'residualDeviance', 'aic', 'mean(resid)','min(resid)','q1(resid)','median(resid)','q3(resid)','max(resid)')
   # dataOut <- data.frame(matrix(ncol = length(stats), nrow = length(cols)))
@@ -211,31 +247,26 @@ glmStats <- function(dataIN, max_p_val=0.20){
   # dataOut$signF1 <- as.character(dataOut$signF1)
   # dataOut$signF2 <- as.character(dataOut$signF2)
   # dataOut$signF3 <- as.character(dataOut$signF3)
-
+  print ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
   dataOut <- data.frame()
   for (col in cols){
-    
-    # print ('#####################################')
-    # print (col)
-    #     print (col)
+    print (col)
     modelF1.model <- bindModel(yLabel='readmitted', xFeatures=col)
-    #     print (modelF1.model)
     modelF1.glm <- glm(modelF1.model, family='binomial',data=dataIN)
-    # print ('popopopopopopop')
     sumry <- getSummaryElements_glm(modelF1.glm, alpha=max_p_val)
-    # print ('iuiuiuiuiuuiuiiu')
+    # print ('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
     # Feature Statistics
     dataOut[col,'numCategories'] <- sumry[["numFeatures"]]
     dataOut[col,'numSignCategories'] <- sumry[["numSignFeatures"]]
     dataOut[col,'signF1'] <- sumry[["topI_SignFeatures"]][1]
     dataOut[col,'signF2'] <- sumry[["topI_SignFeatures"]][2]
     dataOut[col,'signF3'] <- sumry[["topI_SignFeatures"]][3]
-    
+    # print ('cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc')
     # Statistics
     dataOut[col,'nullDeviance'] <- sumry[['nullDeviance']]
     dataOut[col,'residualDeviance'] <- sumry[['residualDeviance']]
     dataOut[col,'aic'] <- sumry[['aic']]
-    
+    # print ('dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd')
     # Residual Statistics
     dataOut[col,'mean(resid)'] <- sumry[["resid_mean"]]
     dataOut[col,'min(resid)'] <- sumry[["resid_min"]]
@@ -250,141 +281,31 @@ glmStats <- function(dataIN, max_p_val=0.20){
   return (dataOut)
 }
 
+inpCols <- c("medical_specialtySurgery_Pediatric", "medical_specialtySurgery_Plastic", "medical_specialtySurgery_PlasticwithinHeadandNeck","readmitted")
+head(trainData)
+trainDataNew <- subset(trainData, select = inpCols)
+head(trainDataNew)
+stats.glm <- glmStats(trainDataNew, max_p_val=0.10)   # Note for most of the cases we exclude diag_1, diag_2, diag_3
+stats.glm
+unique(trainDataNew$medical_specialtySurgery_PlasticwithinHeadandNeck)
+length(unique(trainDataNew$medical_specialtySurgery_Pediatric))
+length(a)
+##################################################################################################
 
-dataDF <- data.frame(subset(trainData, select = -c(diag_1,diag_2,diag_3)))
-dataDF <- subset(trainData, select = c(age,race,time_in_hospital, readmitted))
-stats.glm <- glmStats(dataDF)
+unique()
+stopifnot(colnames(trainData) == colnames(testData))
+# colnames(dataF1_DF)[colSums(is.na(dataF1_DF)) > 0]
 
+colnames()
+head(dataF1_DF)
 
-#######  Check
-
-cols <- colnames(dataDF)
-stats <- c('numCategories', 'numSignCategories', 'signF1','signF2', 'signF3', 'nullDeviance', 'residualDeviance', 'aic', 'mean(resid)','min(resid)','q1(resid)','median(resid)','q3(resid)','max(resid)')
-dataOut <- data.frame(matrix(ncol = length(stats), nrow = length(cols)), stringsAsFactors=FALSE)
-colnames(dataOut) <- stats
-rownames(dataOut) <- cols
-dataOut <- data.frame()
-
-col <- 'time_in_hospital'
-modelF1.glm <- glm(readmitted~time_in_hospital, family='binomial',data=dataDF)
-sumry <- getSummaryElements_glm(modelF1.glm, alpha=0.20)
-
-dataOut[col,'numCategories'] <- sumry[["numFeatures"]]
-dataOut[col,'numSignCategories'] <- sumry[["numSignFeatures"]]
-dataOut[col,'signF1'] <- sumry[["topI_SignFeatures"]][1]
-dataOut[col,'signF2'] <- sumry[["topI_SignFeatures"]][2]
-dataOut[col,'signF3'] <- sumry[["topI_SignFeatures"]][3]
-
-# Statistics
-dataOut[col,'nullDeviance'] <- sumry[['nullDeviance']]
-dataOut[col,'residualDeviance'] <- sumry[['residualDeviance']]
-dataOut[col,'aic'] <- sumry[['aic']]
-
-# Residual Statistics
-dataOut[col,'mean(resid)'] <- sumry[["resid_mean"]]
-dataOut[col,'min(resid)'] <- sumry[["resid_min"]]
-dataOut[col,'q1(resid)'] <- sumry[['resid_q1']]
-dataOut[col,'median(resid)'] <- sumry[['resid_median']]
-dataOut[col,'q3(resid)'] <- sumry[['resid_q3']]
-dataOut[col,'max(resid)'] <- sumry[['resid_max']]
-
-
-
-
-
-
-
-############################################################################################################
-
-col = 'time_in_hospital'
-modelF1.glm <- glm(readmitted~time_in_hospital, family='binomial',data=dataDF)
-inputSummary <- summary(modelF1.glm)
-inputSummary
-
-
-inputSummary_DF <- as.data.frame(inputSummary$coefficients)  # z is for glm
-inputSummary_DF
-inputSummary_DF = inputSummary_DF[-1,] 
-signFeatureIndice <- which(inputSummary_DF['Pr(>|z|)'] <= 0.20)
-signFeatureIndice
-# signFeatureIndice <- signFeatureIndice[signFeatureIndice!=1]
-# signFeatureIndice
-
-numSignFeatures <- length(signFeatureIndice)
-numSignFeatures
-# inputSummary_DF = inputSummary_DF[-1,] 
-i <- 1
-signF1 <- 0
-while (i<=3){
-  # Remove the entry of the intercept term
-  if (nrow(inputSummary_DF) >= 1){
-    min_index <- which(inputSummary_DF['Pr(>|z|)'] == min(inputSummary_DF['Pr(>|z|)']) & inputSummary_DF['Pr(>|z|)'] <= 0.20)
-    min_index
-    if (length(min_index)!=0){
-      signF1[i] <- rownames(inputSummary_DF)[min_index]
-      inputSummary_DF = inputSummary_DF[-min_index,]
-    } else {
-      signF1[i] <- 0
-    }
-  }
-  else{
-    signF1[i] <- 0
-  }
-  i <- i+1
+for (i in colnames(dataF1_DF)){
+  print (i)
+  print (unique(dataF1_DF[[i]]))
 }
 
-signF1[1]
+modelF1.model <- bindModel(yLabel='readmitted', featureSet=names(trainData))
+modelF1.model
+modelF1.glm <- glm(modelF1.model, family = binomial, data=dataF1_DF)
 
-nrow(inputSummary_DF)==0
-
-
-inputSummary_DF = inputSummary_DF[-1,]   # Remove the entry of the intercept term
-min_index <- which(inputSummary_DF['Pr(>|z|)'] == min(inputSummary_DF['Pr(>|z|)']))
-min_index
-signFeature1 <- rownames(inputSummary_DF)[min_index]
-
-inputSummary_DF = inputSummary_DF[-min_index,]
-
-signFeatureIndice <- which(inputSummary_DF['Pr(>|z|)'] <= alpha)
-signFeatureIndice <- signFeatureIndice[signFeatureIndice!=1]   # remo
-
-
-
-
-
-# numFeatures <- nrow(inputSummary_DF) -1 # -1 for the intercept
-# numFeatures
-# signFeatures <- row.names(inputSummary_DF[signFeatureIndice,])
-# signFeatures
-nullDeviance <- inputSummary$null
-nullDeviance
-residualDeviance <- inputSummary$deviance
-residualDeviance
-aic <- inputSummary$aic
-aic
-
-
-residuals <- resid(modelF1.glm)
-mean <- mean(residuals)
-mean
-min <- quantile(residuals)[['0%']]
-min
-q1 <- quantile(residuals)[['25%']]
-q1
-median <- quantile(residuals)[['50%']]
-median
-q3 <- quantile(residuals)[['75%']]
-q3
-max <- quantile(residuals)[['100%']]
-max
-
-
-
-
-min(resid(modelF1.glm))
-mean(resid(modelF1.glm))
-median(resid(modelF1.glm))
-max(resid(modelF1.glm))
-q1(resid(modelF1.glm))
-
-sumry <- getSummaryElements_glm(summary(modelF1.glm), alpha=0.20)
+head(dataF1_DF$readmitted)
